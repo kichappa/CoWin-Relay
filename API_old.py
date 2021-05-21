44109679797a9c136d11a431f1573a57d53dd687
Kishore S. Shenoy <kichappa@gmail.com>
2021-05-21 21:49:02 +0530

    Update API_old.py

--- API_old.py
+++ API_old.py
@@ -11,38 +11,38 @@ print(token)
 with open("mobile.json", "r") as f:
     mobile=json.load(f)['mobile']
 
-URL = "https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP"
-response = requests.post(URL, json={
-    "mobile": mobile,
-    "secret": token
-}, headers={
-    "accept": "application/json",
-    "Accept-Language": "en_US",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
-    })
+# URL = "https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP"
+# response = requests.post(URL, json={
+#     "mobile": mobile,
+#     "secret": token
+# }, headers={
+#     "accept": "application/json",
+#     "Accept-Language": "en_US",
+#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+#     })
 
-print(response)
-print(response.json())
-otp=str(input("Enter OTP: "))
-otpHash=hashlib.sha256(otp.encode()).hexdigest()
-print(otp, otpHash)
+# print(response)
+# print(response.json())
+# otp=str(input("Enter OTP: "))
+# otpHash=hashlib.sha256(otp.encode()).hexdigest()
+# print(otp, otpHash)
 
-URL = "https://cdn-api.co-vin.in/api/v2/auth/validateMobileOtp"
-response = requests.post(URL, json={
-    "otp": otpHash,
-    "txnId": response.json()['txnId']
-}, headers={
-    "accept": "application/json",
-    "Accept-Language": "en_US",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
-    })
+# URL = "https://cdn-api.co-vin.in/api/v2/auth/validateMobileOtp"
+# response = requests.post(URL, json={
+#     "otp": otpHash,
+#     "txnId": response.json()['txnId']
+# }, headers={
+#     "accept": "application/json",
+#     "Accept-Language": "en_US",
+#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+#     })
 
-print(response)
-print(response.json())
+# print(response)
+# print(response.json())
 
-bearer_token=response.json()['token']
-with open("token.json", 'w+') as f:
-    json.dump(response.json(),f, indent=2)
+# bearer_token=response.json()['token']
+# with open("token.json", 'w+') as f:
+#     json.dump(response.json(),f, indent=2)
 
 with open("token.json", 'r') as f:
     response = json.load(f)
@@ -58,6 +58,17 @@ response = requests.get(URL, headers={
 
 print(response)
 print(response.json())
+
+URL = "https://cdn-api.co-vin.in/api/v2/appointment/centers/findByLatLong?lat=27&long=76"
+response = requests.get(URL, headers={
+    "accept": "application/json",
+    "Accept-Language": "en_US",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "authorization": "Bearer {}".format(bearer_token)
+    })
+
+print(response)
+print(response.content)
 
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/beneficiaries"
 response = requests.get(URL, headers={
