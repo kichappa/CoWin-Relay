d7a462e4de03f10e90bcfaf14ab9373a28e9a410
Kishore S. Shenoy <kichappa@gmail.com>
2021-05-21 20:10:03 +0530

    Updated API methods

--- API_old.py
+++ API_old.py
@@ -1,4 +1,4 @@
-import requests, json, hashlib, pathlib, datetime
+import requests, json, hashlib, pathlib
 from secret import encrypt
 
 password = "CoWIN@$#&*(!@%^&".encode()
@@ -7,133 +7,80 @@ username="b5cab167-7977-4df1-8027-a63aa144f04e".encode()
 token=encrypt(username, password).decode()
 print(token)
 
-def _get_response(method, headerAppend={}, **kwargs):
-    with open("api.json", "r") as f:
-        apiJSON=json.load(f)
-        request={**apiJSON[method], "header": {**apiJSON["genericHeaders"], **headerAppend}}
-
-    
-    for key, value in kwargs.items():
-        try:
-            if type(request[key])==str:
-                request[key]=request[key].format(**value)
-        except Exception as e:
-            print(e)
-        
-    if request["type"]=="POST":
-        request["json"]=kwargs["json"]
-
-    # print(json.dumps(request, indent=2))
-
-    if request["type"]=="POST":
-        return requests.post(url=request['url'], json=request['json'], headers=request['header'])
-    elif request["type"]=="GET":
-        return requests.get(url=request['url'], headers=request['header'])
-
-def _today():
-    return "{}-{}-{}".format(datetime.date.today().day, datetime.date.today().month, datetime.date.today().year)
 
 with open("mobile.json", "r") as f:
     mobile=json.load(f)['mobile']
-    
-response = _get_response("generateMobileOTP", json={
+
+URL = "https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP"
+response = requests.post(URL, json={
     "mobile": mobile,
     "secret": token
-})
+}, headers={
+    "accept": "application/json",
+    "Accept-Language": "en_US",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    })
 
 print(response)
-try:
-    print(response.json())
-except:
-    print(response.content)
-
+print(response.json())
 otp=str(input("Enter OTP: "))
 otpHash=hashlib.sha256(otp.encode()).hexdigest()
 print(otp, otpHash)
 
-response = _get_response("validateMobileOTP", json={
+URL = "https://cdn-api.co-vin.in/api/v2/auth/validateMobileOtp"
+response = requests.post(URL, json={
     "otp": otpHash,
     "txnId": response.json()['txnId']
-})
+}, headers={
+    "accept": "application/json",
+    "Accept-Language": "en_US",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    })
 
 print(response)
-try:
-    print(response.json())
-except:
-    print(response.content)
+print(response.json())
 
 bearer_token=response.json()['token']
 with open("token.json", 'w+') as f:
     json.dump(response.json(),f, indent=2)
 
-# with open("token.json", 'r') as f:
-#     response = json.load(f)
-# bearer_token=response['token']
+with open("token.json", 'r') as f:
+    response = json.load(f)
+bearer_token=response['token']
 
-response = _get_response("calendarByDistrict", headerAppend={
+URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=307&date=20-05-2021"
+response = requests.get(URL, headers={
+    "accept": "application/json",
+    "Accept-Language": "en_US",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
     "authorization": "Bearer {}".format(bearer_token)
-}, url={
-    "district_id": 307, 
-    "date": _today()
-})
+    })
 
-print("\n\nCalender By District")
-print(response)
-try:
-    print(response.json())
-except:
-    print(response.content)
-
-response = _get_response("findByDistrict", headerAppend={
-    "authorization": "Bearer {}".format(bearer_token)
-}, url={
-    "district_id": 307, 
-    "date": _today()
-})
-
-print("\n\nFind By District")
 print(response)
 print(response.json())
 
-response = _get_response("calendarByPIN", headerAppend={
+URL = "https://cdn-api.co-vin.in/api/v2/appointment/beneficiaries"
+response = requests.get(URL, headers={
+    "accept": "application/json",
+    "Accept-Language": "en_US",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
     "authorization": "Bearer {}".format(bearer_token)
-}, url={
-    "pincode": 682024, 
-    "date": _today()
-})
+    })
 
-print("\n\nCalender By PIN")
 print(response)
 print(response.json())
 
-response = _get_response("findByPIN", headerAppend={
+URL = "https://cdn-api.co-vin.in/api/v2/registration/certificate/download?beneficiary_reference_id={}".format(response.json()['beneficiaries'][0]['beneficiary_reference_id'])
+response = requests.get(URL, headers={
+    "accept": "application/json",
+    "Accept-Language": "en_US",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
     "authorization": "Bearer {}".format(bearer_token)
-}, url={
-    "pincode": 682024, 
-    "date": _today()
-})
-
-print("\n\nFind By PIN")
-print(response)
-print(response.json())
-
-response = _get_response("beneficiaries", headerAppend={
-    "authorization": "Bearer {}".format(bearer_token)
-})
+    })
 
 print(response)
-try:
-    print(response.json())
-except:
-    print(response.content)
-    
-URL = "https://cdn-api.co-vin.in/api/v2/registration/certificate/download?beneficiary_reference_id={}".format(response.json()['beneficiaries'][1]['beneficiary_reference_id'])
-response = _get_response("certificate", headerAppend={
-    "authorization": "Bearer {}".format(bearer_token)
-}, url={
-    "beneficiary_reference_id": response.json()['beneficiaries'][1]['beneficiary_reference_id']
-})
-
-print(response)
+print(response.raw)
+# print(response.content)
 with pathlib.Path("beni.pdf") as f:
     f.write_bytes(response.content)
+# print(response.json())
